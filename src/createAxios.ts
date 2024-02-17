33c9124a13cb4f7db576b7b8b02e4059242b3467
Ahmed Chakhoum <rainxh11@gmail.com>
2024-02-17 15:16:38 +0100

    + added error & response type, error logging

--- src/createAxios.ts
+++ src/createAxios.ts
@@ -1,33 +1,35 @@
-import axios, { AxiosInstance, AxiosResponse, CreateAxiosDefaults } from 'axios'
+import axios, {
+	type AxiosError,
+	type AxiosInstance,
+	type AxiosResponse,
+	type CreateAxiosDefaults,
+} from "axios";
 
 export function createAxios(
-  configInstanceFn: (instance: AxiosInstance) => AxiosInstance,
-  config?: CreateAxiosDefaults,
-  errorLoggingFn?: (error: unknown) => void,
+	configInstanceFn: (instance: AxiosInstance) => AxiosInstance,
+	config?: CreateAxiosDefaults,
+	logErrFn?: (error: AxiosError<unknown>) => void,
 ) {
-  const client = configInstanceFn(axios.create(config))
+	const client = configInstanceFn(axios.create(config));
 
-  function createRequestFn<T>(
-    instanceFn: (
-      instance: AxiosInstance,
-      args?: unknown,
-      signal?: AbortSignal,
-    ) => Promise<AxiosResponse<T>>,
-  ): (args?: unknown, signal?: AbortSignal) => Promise<T> {
-    return (args?: unknown, signal?: AbortSignal) => {
-      return new Promise<T>((resolve, reject) => {
-        instanceFn(client, args ?? {}, signal)
-          .then(res =>
-            res.status >= 400
-              ? reject('🔴 Unsuccessful Response, Status Code:' + res.status)
-              : resolve(res?.data),
-          )
-          .catch(error => {
-            if (errorLoggingFn) errorLoggingFn(error)
-            reject(error)
-          })
-      })
-    }
-  }
-  return { client, createRequestFn }
+	function createRequestFn<TInput, TResponse, TError = unknown>(
+		instanceFn: (
+			instance: AxiosInstance,
+			args?: TInput,
+			signal?: AbortSignal,
+		) => Promise<AxiosResponse<unknown>>,
+	): (args?: TInput, signal?: AbortSignal) => Promise<TResponse> {
+		return (args?: TInput, signal?: AbortSignal) => {
+			return new Promise<TResponse>((resolve, reject) => {
+				instanceFn(client, args, signal)
+					.then((res) => resolve(res?.data as TResponse))
+					.catch((error) => {
+						if (logErrFn) logErrFn(error as AxiosError<TError>);
+						reject((error as AxiosError<TError>).response?.data);
+					});
+			});
+		};
+	}
+
+	return { client, createRequestFn };
 }
